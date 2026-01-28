d661683ab1ebdc6275ec3730585ec7069b1c49db
dellie <delsterone@gmail.com>
2026-01-28 17:17:03 -0600

    login working, need to collect username & password or smth for verifying who ppl are

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,14 @@
 import { betterAuth } from "better-auth"
 import "dotenv/config";
 
-console.log(process.env.AUTH_GITHUB_ID as string)
+export const d3_ID = process.env.D3_ID!;
+console.log("CLIENT ID: " + process.env.AUTH_GITHUB_ID as string)
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:4321",
   secret: process.env.AUTH_SECRET!,
   socialProviders: {
     github: {
-      clientId: process.env.AUTH_GITHUB_ID!,
+      clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
     },
   },
