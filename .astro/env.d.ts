228db2716a703752ac4f01e4e17c43da34f9a8b1
dellie <delsterone@gmail.com>
2026-02-17 11:58:59 -0600

    more updates

--- .astro/env.d.ts
+++ .astro/env.d.ts
@@ -5,7 +5,7 @@ declare module 'astro:env/server' {
 	export const AUTH_GITHUB_SECRET: string;	
 	export const D3_EMAIL: string | undefined;	
 	export const ALLOWED_EMAILS: string | undefined;	
-	export const BETTER_AUTH_DATABASE_URL: string | undefined;	
+	export const DATABASE_URL: string | undefined;	
 	export const DB_PASSWORD: string | undefined;	
 	export const PGPASSWORD: string | undefined;	
 }
--- src/pages/api/auth/[...all].ts
+++ src/pages/api/auth/[...all].ts
@@ -1,9 +1,9 @@
 import type { APIRoute } from "astro";
-import { auth } from "../../../auth";
+import { createAuth } from "../../../auth";
 
 export const ALL: APIRoute = async (ctx) => {
+  const auth = createAuth(ctx.locals.runtime.env);
   return auth.handler(ctx.request);
 };
 
 export const prerender = false;
-
